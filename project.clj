8110f28c980824d07248470a82195b8cecf4f7cb
Danny Wilson <danny@prime.vc>
2015-11-11 13:57:33 +0100

    Fix dependencies and add JRuby command aliases

--- project.clj
+++ project.clj
@@ -2,7 +2,11 @@
   :description "Clojure Ring handler implementation of Ruby's Rack webapp spec"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.jruby/jruby "9.0.0.0"]
-                 [org.jruby.rack/jruby-rack "1.1.18"]
+                 [org.jruby.rack/jruby-rack "1.1.18" :exclusions [org.jruby/jruby-complete]]
                  [javax.servlet/servlet-api "2.5"]
                  [zweikopf "1.0.1" :exclusions [org.jruby/jruby-complete]]
-                ])
+                ]
+  ;; PATH=/tmp/bundler/bin:$PATH GEM_HOME=/tmp/bundler lein gem install bundler
+  :aliases {"gem"    ["run" "-m" "org.jruby.Main" "-S" "gem"]
+            "bundle" ["run" "-m" "org.jruby.Main" "-S" "bundle"]
+            "rails"  ["run" "-m" "org.jruby.Main" "-S" "rails"]})
